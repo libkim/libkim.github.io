2195383a42fff6481195c88b65160a9743c46b59
libkim <90676303+libkim@users.noreply.github.com>
2024-02-03 20:52:03 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -36,8 +36,8 @@ for tid in tids:
     '#main > h1'
   ).get_text()
   ani['ko-title'] = None
-  premiered = soup.select_one('#tidContainer > div.tidTabContentLayout > div.tidSidebar > div > table.section.basic > tbody > tr > td > table > tbody > tr:nth-child(3) > td').get_text()
-  ani['premiered'] = premiered.split('～')[0]
+  premiered = "{}".format(soup.select_one('#tidContainer > div.tidTabContentLayout > div.tidSidebar > div > table.section.basic > tbody > tr > td > table > tbody > tr:nth-child(3) > td'))
+  ani['premiered'] = premiered.get_text().split('～')[0]
   ani['bookmark'] = None
   ani['follow-ups'] = follow_ups
   ani_list.append(ani)
