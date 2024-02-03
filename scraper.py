22cf6dcc750939e02a5d0539de4e28e0d536b02f
libkim <90676303+libkim@users.noreply.github.com>
2024-02-03 17:47:45 +0900

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+tids = [1597, 3380, 6793]
+ani_list = []
+
+for tid in tids:
+  req = requests.get(f'https://cal.syoboi.jp/tid/{tid}')
+  req.encoding = None
+  html = req.content
+  soup = BeautifulSoup(html, 'html.parser')
+  
+  ani = {}
+  follow_ups = []
+  
+  follow_ups_path = soup.select(
+    '#tid_summary > div:nth-child(2) > ul'
+  ).find_all('li')
+  for path in follow_ups_path
+    follow_up = {}
+    follow_up['title'] = path.find('a').get_text()
+    follow_up['ko-title'] = None
+    follow_up['premiered'] = path.find('a').decompose().get_text()
+    follow_up['bookmark'] = None
+    follow_ups.append(follow_up)
+
+  ani['title'] = soup.select(
+    '#main > h1'
+  ).get_text()
+  ani['ko-title'] = None
+  ani['premiered'] = soup.select(
+    '#tidContainer > div.tidTabContentLayout > div.tidSidebar > div > table.section.basic > tbody > tr > td > table > tbody > tr:nth-child(3) > td'
+  ).get_text().split('～')[0]
+  ani['bookmark'] = None
+  ani['follow-ups'] = follow_ups
+  ani_list.append(ani)
+
+with open(os.path.join(BASE_DIR, 'ani-list.json'), 'w+',encoding='utf-8') as json_file:
+    json.dump(ani_list, json_file, ensure_ascii = False, indent='\t')
