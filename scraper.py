db560220aebbbde030f6f026b5bbf7cd2e9154fc
libkim <90676303+libkim@users.noreply.github.com>
2024-02-05 17:29:29 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,21 +7,21 @@ import sys
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 tids = [1597, 3380, 6793, 2003]
-
 ani_list = []
-ani = {}
-follow_ups = []
-follow_up = {}
 
 for tid in tids:
   url = f'https://cal.syoboi.jp/tid/{tid}/summary'
   html = requests.get(url)
   soup = BeautifulSoup(html.text, 'html.parser')
 
+  ani = {}
+  follow_ups = []
+  
   follow_ups_path = soup.find(
     'ul', class_='tidList'
   ).find_all('li')
   for path in follow_ups_path:
+    follow_up = {}
     if path.select_one('a'):
       follow_up['title'] = path.select_one('a').extract().get_text()
       follow_up['ko-title'] = None
