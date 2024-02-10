18a57c05c9bc0ef2086cb10379f2445df878b363
libkim <90676303+libkim@users.noreply.github.com>
2024-02-10 12:39:05 +0900

    scraper.py 업데이트

--- scraper.py
+++ scraper.py
@@ -54,7 +54,7 @@ for ani in ani_list:
     if not 'ko-title' in ani: # 키가 없으면
       ani['ko-title'] = None
       
-    premiered = soup.find('table', class_='data').find_all('tr')[2].select_one('td').get_text().split('～')[0].split('-') # 무조건 다시 생성
+    premiered = soup.find('table', class_='data').find_all('tr')[2].select_one('td').string.split('～')[0].split('-') # 무조건 다시 생성
     ani['premiered'] = premiered[0] + '-' + premiered[1].zfill(2)
 
     if not 'bookmark' in ani: # 키가 없으면
