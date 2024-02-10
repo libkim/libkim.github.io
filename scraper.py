d5e73692d822fbe0206f8d9cb1dfd092e143116e
libkim <90676303+libkim@users.noreply.github.com>
2024-02-10 13:22:47 +0900

    scraper.py 업데이트

--- scraper.py
+++ scraper.py
@@ -55,7 +55,7 @@ for ani in ani_list:
       ani['ko-title'] = None
       
     premiered = soup.find('th', text='放送期間').parent.select_one('td').string.split('～')[0].split('-') # 무조건 다시 생성
-    ani['premiered'] = premiered[1]
+    ani['premiered'] = premiered[0] + '-' + premiered[1].zfill(2)
 
     if not 'bookmark' in ani: # 키가 없으면
       ani['bookmark'] = None
