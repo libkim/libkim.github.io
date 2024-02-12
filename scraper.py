882ed84a39b22b82eb3961607017f22529fc4474
libkim <90676303+libkim@users.noreply.github.com>
2024-02-12 14:07:13 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,7 +1,6 @@
+import os
 import requests
 from bs4 import BeautifulSoup
-import os
-import sys
 from ruamel.yaml import YAML
 from operator import itemgetter
 
@@ -11,27 +10,26 @@ updated_ani_list = []
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 KEY_ORDER = ['tid', 'title', 'ko-title', 'premiered', 'bookmark', 'follow-ups']
 
-def create_tid():
-  if 'title' in ani:
-    url = f"https://cal.syoboi.jp/find?kw={ani['title']}"
-    html = requests.get(url)
-    soup = BeautifulSoup(html.text, 'html.parser')
-    ani['tid'] = int(soup.find('a', string=f"{ani['title']}")['href'].split('/')[2])
-
 def strip_leading_double_space(stream):
   if stream.startswith("  "):
     stream = stream[2:]
   return stream.replace("\n  ", "\n")
+  
+def create_tid():
+  url = f"https://cal.syoboi.jp/find?kw={ani['title']}"
+  html = requests.get(url)
+  soup = BeautifulSoup(html.text, 'html.parser')
+  ani['tid'] = int(soup.find('a', string=f"{ani['title']}")['href'].split('/')[2])
 
 yaml = YAML()
 with open(os.path.join(BASE_DIR, 'ani-list.yml')) as file:
   ani_list = yaml.load(file)
 
 for ani in ani_list:
-  if not 'tid' in ani or ani['tid'] == None or type(ani['tid']) != 'int': # 키가 없거나 키는 있지만 값이 없거나 값이 있지만 값이 정수가 아니면
+  if not 'tid' in ani or type(ani['tid']) != 'int' and 'title' in ani: # 키가 없거나 키는 있지만 값이 정수가 아니면, 타이틀이 존재하면
     create_tid()
   
-  if 'tid' in ani:
+  if 'tid' in ani and type(ani['tid']) == 'int':
     url = f"https://cal.syoboi.jp/tid/{ani['tid']}/summary"
     html = requests.get(url)
     soup = BeautifulSoup(html.text, 'html.parser')
@@ -69,7 +67,8 @@ for ani in ani_list:
         updated_follow_up['premiered'] = path.get_text(strip=True).replace('月','').replace('年','-')
         updated_follow_up['bookmark'] = next((follow_up.get('bookmark') for follow_up in ani.get('follow-ups', []) if follow_up['title'] == updated_follow_up['title']), None)
         updated_follow_ups.append(updated_follow_up)
-    ani['follow-ups'] = sorted(updated_follow_ups, key=itemgetter('tid'))
+    ani['follow-ups'] = sorted(updated_follow_ups, key=itemgetter('premiered'))
+    
   index_map = {key: index for index, key in enumerate(KEY_ORDER)}
   sorted_ani = dict(sorted(ani.items(), key=lambda k: index_map[k[0]]))
   updated_ani_list.append(sorted_ani)
