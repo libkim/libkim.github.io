d5acd3472a5c65a45b130173db8d92de33a06e00
libkim <90676303+libkim@users.noreply.github.com>
2024-04-11 01:59:37 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -43,22 +43,29 @@ for ani in ani_list:
       soup = BeautifulSoup(html.text, 'html.parser')
       follow_up_paths = soup.find('ul', class_='tidList').find_all('li')
 
-    updated_follow_ups = []
-    
+    # title 업데이트
     if soup.select_one('#main > h1 > span'):
       soup.select_one('#main > h1 > span').decompose()
       soup.select_one('#main > h1 > a').decompose()
     ani['title'] = soup.select_one('#main > h1').get_text(strip=True) # 무조건 다시 생성
-    
-    if not 'ko-title' in ani: # 키가 없으면
+
+    # ko-title 업데이트
+    if not 'ko-title' in ani:
       ani['ko-title'] = None
-      
+    print(ani['ko-title'])
+
+    # premiered 업데이트
     premiered = soup.find('th', string='放送期間').parent.select_one('td').get_text().split('～')[0].split('-') # 무조건 다시 생성
     ani['premiered'] = premiered[0] + '-' + premiered[1].zfill(2)
+    print(ani['premiered'])
 
-    if not 'bookmark' in ani: # 키가 없으면
+    # bookmark 업데이트
+    if not 'bookmark' in ani:
       ani['bookmark'] = None
+    print(ani['bookmark'])
 
+    # follow-ups 업데이트
+    updated_follow_ups = []
     for path in follow_up_paths:
       updated_follow_up = {}
       if path.select_one('a'):
@@ -69,10 +76,15 @@ for ani in ani_list:
         updated_follow_up['bookmark'] = next((follow_up.get('bookmark') for follow_up in ani.get('follow-ups', []) if follow_up['title'] == updated_follow_up['title']), None)
         updated_follow_ups.append(updated_follow_up)
     ani['follow-ups'] = sorted(updated_follow_ups, key=itemgetter('premiered'))
+    print(ani['follow-ups'])
     
-  index_map = {key: index for index, key in enumerate(KEY_ORDER)}
-  sorted_ani = dict(sorted(ani.items(), key=lambda k: index_map[k[0]]))
-  updated_ani_list.append(sorted_ani)
+  # end if 'tid' in ani and type(ani['tid']) == 'int':
+# end for ani in ani_list:
+
+# 정렬 후 저장
+index_map = {key: index for index, key in enumerate(KEY_ORDER)}
+sorted_ani = dict(sorted(ani.items(), key=lambda k: index_map[k[0]]))
+updated_ani_list.append(sorted_ani)
 
 with open('ani-list.yml', 'w') as file:
   yaml = YAML()
